96d949f823c9c2d3599752060c62ac693ac5c843
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-11-05 18:41:23 -0500

    feat(counter): add counter function with closure.

--- src/counter.js
+++ src/counter.js
@@ -1,1 +1,30 @@
+const countText = document.querySelector(".count");
 
+function setCounter() {
+  function setText() {
+    countText.textContent = count;
+  }
+
+  let count = 0;
+
+  function increment() {
+    count++;
+    setText();
+  }
+  function decrement() {
+    count--;
+    setText();
+  }
+  function reset() {
+    count = 0;
+    setText();
+  }
+
+  return {
+    increment,
+    decrement,
+    reset,
+  };
+}
+
+export default setCounter;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+import setCounter from "./counter";
+
+const incrementBtn = document.querySelector(".button__increment");
+const decrementBtn = document.querySelector(".button__decrement");
+const resetBtn = document.querySelector(".button__reset");
+
+const myCounter = setCounter();
+
+incrementBtn.addEventListener("click", () => myCounter.increment());
+decrementBtn.addEventListener("click", () => myCounter.decrement());
+resetBtn.addEventListener("click", () => myCounter.reset());
